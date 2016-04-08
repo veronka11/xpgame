cb1d5af19eb782e8f7ba1e39546ead58231b8080
Ondrej Husar <ondrej@husar.sk>
2016-04-08 10:14:36 +0200

    remove: try catch block from JSONloader
    * It now throws FileNotFoundException, which seems more logical to me
    * It is very bad practice to use catch(Exception e) @stiffix

--- src/dataloader/JSONloader.java
+++ src/dataloader/JSONloader.java
@@ -4,6 +4,7 @@ import entity.Building;
 import entity.BuildingUpgrade;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,80 +15,75 @@ import javax.json.JsonReader;
 
 public class JSONloader {
     
-    public static HashMap<Integer, Building> JSONloadBuildings(String path) {
+    public static HashMap<Integer, Building> JSONloadBuildings(String path) throws FileNotFoundException {
         // Result variables
         HashMap<Integer, Building> data = new HashMap<Integer, Building>();
-        
-        try {
-            // Get File from path and init JSONparser
-            InputStream in = new FileInputStream(new File(path));
-            JsonReader jreader = Json.createReader(in);
-            
-            JsonObject obj = jreader.readObject();
-            
-            // Building processing variables
-            String name;
-            boolean productivity;
-            int[] price, production;
-            ArrayList<BuildingUpgrade> buildingUpgrades;
-            BuildingUpgrade tempBuildingUpgrade;
-            
-            // Buildings
-            int id = 0;
-            JsonArray buildings = obj.getJsonArray("buildings");
-            for (JsonObject building : buildings.getValuesAs(JsonObject.class)) {
-                // Name
-                name = building.getString("name");
-                
-                // Instance
-                Building tempBuilding = new Building(id, name);
-                
-                // Price
-                price = new int[building.size()];
-                JsonArray tempPrice = building.getJsonArray("price");
-                for (int i = 0; i < tempPrice.size(); i++) {
-                        price[i] = tempPrice.getInt(i);
-                }
-                tempBuilding.setPrice(price);
-                
-                // Production
-                productivity = building.getBoolean("productive");
-                if (productivity) {
-                    JsonArray tempProduction = building.getJsonArray("production");
-                    production = new int[tempProduction.size()];
-                    for (int i = 0; i < tempProduction.size(); i++) {
-                        production[i] = tempProduction.getInt(i);
-                    }
-                    tempBuilding.setProduction(production);
-                } else {
-                    tempBuilding.setProduction(productivity);
-                }
-                
-                // BuildingUpgrades
-                buildingUpgrades = new ArrayList<>();
-                JsonArray tempUpgrades = building.getJsonArray("upgrades");
-                for (JsonObject upgr : tempUpgrades.getValuesAs(JsonObject.class)) {
-                    tempBuildingUpgrade = new BuildingUpgrade(upgr.getString("upgrade_name"), upgr.getInt("upgrade_type"), Double.valueOf(upgr.get("upgrade_rate").toString()), Double.valueOf(upgr.get("upgrade_level_increase").toString()));
-                    buildingUpgrades.add(tempBuildingUpgrade);
-                }
-                tempBuilding.setUpgrades(buildingUpgrades);
-                
-                // ----------------------------
-                // Add building
-                data.put(id, tempBuilding);
-                id++;
-                
-                System.out.println(tempBuilding.toString());
+
+
+        // Get File from path and init JSONparser
+        InputStream in = new FileInputStream(new File(path));
+        JsonReader jreader = Json.createReader(in);
+
+        JsonObject obj = jreader.readObject();
+
+        // Building processing variables
+        String name;
+        boolean productivity;
+        int[] price, production;
+        ArrayList<BuildingUpgrade> buildingUpgrades;
+        BuildingUpgrade tempBuildingUpgrade;
+
+        // Buildings
+        int id = 0;
+        JsonArray buildings = obj.getJsonArray("buildings");
+        for (JsonObject building : buildings.getValuesAs(JsonObject.class)) {
+            // Name
+            name = building.getString("name");
+
+            // Instance
+            Building tempBuilding = new Building(id, name);
+
+            // Price
+            price = new int[building.size()];
+            JsonArray tempPrice = building.getJsonArray("price");
+            for (int i = 0; i < tempPrice.size(); i++) {
+                price[i] = tempPrice.getInt(i);
             }
-        } catch (Exception e) { e.printStackTrace(); }
-        
+            tempBuilding.setPrice(price);
+
+            // Production
+            productivity = building.getBoolean("productive");
+            if (productivity) {
+                JsonArray tempProduction = building.getJsonArray("production");
+                production = new int[tempProduction.size()];
+                for (int i = 0; i < tempProduction.size(); i++) {
+                    production[i] = tempProduction.getInt(i);
+                }
+                tempBuilding.setProduction(production);
+            } else {
+                tempBuilding.setProduction(productivity);
+            }
+
+            // BuildingUpgrades
+            buildingUpgrades = new ArrayList<>();
+            JsonArray tempUpgrades = building.getJsonArray("upgrades");
+            for (JsonObject upgr : tempUpgrades.getValuesAs(JsonObject.class)) {
+                tempBuildingUpgrade = new BuildingUpgrade(upgr.getString("upgrade_name"), upgr.getInt("upgrade_type"), Double.valueOf(upgr.get("upgrade_rate").toString()), Double.valueOf(upgr.get("upgrade_level_increase").toString()));
+                buildingUpgrades.add(tempBuildingUpgrade);
+            }
+            tempBuilding.setUpgrades(buildingUpgrades);
+
+            // ----------------------------
+            // Add building
+            data.put(id, tempBuilding);
+            id++;
+        }
         return data;
     }
-    
-    
+
     // Main
     public static void main(String[] args) {
-        JSONloader.JSONloadBuildings("/Users/newnew/NetBeansProjects/xpgame/XPgame/src/dataloader/structure.json");
+//        JSONloader.JSONloadBuildings("/Users/newnew/NetBeansProjects/xpgame/XPgame/src/dataloader/structure.json");
     }
     
     
