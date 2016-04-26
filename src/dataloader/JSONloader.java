fd9cec14274aecb30659660bf62e1fce63cbe8b0
stiffix <stiffsoftware@gmail.com>
2016-04-26 22:14:20 +0200

    Add JSONloader saving function to json, reordered Commodity enum, updated structure json

--- src/dataloader/JSONloader.java
+++ src/dataloader/JSONloader.java
@@ -2,20 +2,85 @@ package dataloader;
 
 import entity.Building;
 import entity.BuildingUpgrade;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.DoubleSummaryStatistics;
-import java.util.HashMap;
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
+import xpgame.Commodity;
+
+import java.io.*;
+import java.util.*;
+import javax.json.*;
+import javax.json.stream.JsonGenerationException;
+import javax.json.stream.JsonGenerator;
 
 public class JSONloader {
-    
+
+    private final static String PATH_TO_SAVE = System.getProperty("user.dir") + "/savedata";
+
+    public static  boolean JSONsaveBuildings(HashMap<Integer, Building> data) {
+        System.out.println(PATH_TO_SAVE);
+        System.out.println(Commodity.FOOD.ordinal());
+
+        // Init stream writers
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(PATH_TO_SAVE + "test.json");
+
+            JsonGenerator gen = Json.createGenerator(writer);
+
+            gen.writeStartObject().writeStartArray("buildings");
+
+            // Building data
+            Building tempBuilding;
+            Iterator it = data.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry pair = (Map.Entry)it.next();
+                tempBuilding = (Building)pair.getValue();
+
+                gen.writeStartObject()
+                        .write("id", tempBuilding.getId())
+                        .write("name", tempBuilding.getName())
+                        .writeStartArray("price")
+                            .write(tempBuilding.getPrice()[Commodity.FOOD.ordinal()])
+                            .write(tempBuilding.getPrice()[Commodity.GOLD.ordinal()])
+                            .write(tempBuilding.getPrice()[Commodity.STONE.ordinal()])
+                            .write(tempBuilding.getPrice()[Commodity.WOOD.ordinal()])
+                        .writeEnd()
+                        .write("productive", tempBuilding.isProductive());
+
+
+                if (tempBuilding.isProductive()) {
+                    gen.writeStartArray("production")
+                            .write(tempBuilding.getProduction()[Commodity.FOOD.ordinal()])
+                            .write(tempBuilding.getProduction()[Commodity.GOLD.ordinal()])
+                            .write(tempBuilding.getProduction()[Commodity.STONE.ordinal()])
+                            .write(tempBuilding.getProduction()[Commodity.WOOD.ordinal()])
+                        .writeEnd();
+                } else {
+                    gen.writeStartArray("production").writeEnd();
+                }
+
+                gen.writeStartObject("upgrade")
+                        .write("upgrade_name", tempBuilding.getUpgrade().getName())
+                        .write("upgrade_type", tempBuilding.getUpgrade().getType())
+                        .write("upgrade_rate", tempBuilding.getUpgrade().getRate())
+                        .write("upgrade_level_increase", tempBuilding.getUpgrade().getLevelIncrease())
+                    .writeEnd()
+                .writeEnd();
+
+            }
+
+            // End of Array Buildings and closing object }
+            gen.writeEnd().writeEnd();
+            gen.close();
+        } catch (IOException e) {
+            return false;
+        } catch (JsonGenerationException jge) {
+            return false;
+        } catch (JsonException je) {
+            return false;
+        }
+
+        return true;
+    }
+
     public static HashMap<Integer, Building> JSONloadBuildings(String path) throws FileNotFoundException {
         // Result variables
         HashMap<Integer, Building> data = new HashMap<Integer, Building>();
@@ -77,6 +142,7 @@ public class JSONloader {
             // ----------------------------
             // Add building
             data.put(id, tempBuilding);
+            //System.out.println(tempBuilding);
         }
         return data;
     }
@@ -84,7 +150,9 @@ public class JSONloader {
     // Main
     public static void main(String[] args) {
         try {
-            JSONloader.JSONloadBuildings("/Users/newnew/IdeaProjects/xpgame/src/dataloader/structure.json");
+            HashMap<Integer, Building> hm = JSONloader.JSONloadBuildings("/Users/newnew/IdeaProjects/xpgame/src/dataloader/structure.json");
+            //HashMap<Integer, Building> hm = JSONloader.JSONloadBuildings("/Users/newnew/IdeaProjects/xpgame/savedatatest.json");
+            //JSONloader.JSONsaveBuildings(hm);
         } catch (FileNotFoundException fnfe) {
             fnfe.printStackTrace();
         }
