5421537fb2c4eb4be2adae3a67b2e6301191b4fe
Ondrej Husar <ondrej@husar.sk>
2016-04-27 08:06:05 +0200

    remove generated bullshit from test files

--- test/Buildings/BuildingsSuite.java
+++ test/Buildings/BuildingsSuite.java
@@ -19,21 +19,5 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({Buildings.GameBuildingControllerTest.class, Buildings.GameBuildingTest.class})
 public class BuildingsSuite {
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
     
 }
