c43eb05eeb2d27fb6fb335cfd54e4eec208079cb
Ondrej Husar <ondrej@husar.sk>
2016-04-27 08:06:05 +0200

    remove generated bullshit from test files

--- test/Buildings/GameBuildingTest.java
+++ test/Buildings/GameBuildingTest.java
@@ -17,25 +17,5 @@ import static org.junit.Assert.*;
  * @author peto
  */
 public class GameBuildingTest {
-    
-    public GameBuildingTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    
 }
